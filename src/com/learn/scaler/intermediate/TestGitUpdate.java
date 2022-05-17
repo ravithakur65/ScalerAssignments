8982412a0ba8f21c0f6a52b83941186f7e3a35e8
KJ783CX <KJ783CX@IN3057095W1.ey.net>
2022-05-17 19:06:55 +0530

    Committing new class fine in branchX

---
+++ src/com/learn/scaler/intermediate/TestGitUpdate.java
@@ -0,0 +1,10 @@
+package com.learn.scaler.intermediate;
+
+public class TestGitUpdate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing GIT update");
+	}
+
+}
